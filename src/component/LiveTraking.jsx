67d996efc4f19cbb3aa4d7a40db99f50615ad90f
SiddheshDhinge <saddy97531@gmail.com>
2023-11-11 00:07:48 +0530

    Functional it is

--- src/component/LiveTraking.jsx
+++ src/component/LiveTraking.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { useState } from "react";
 import {
 	Map,
 	GoogleApiWrapper,
@@ -6,22 +7,28 @@ import {
 	Marker,
 } from "google-maps-react-18-support";
 
-class LiveTraking extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			source: "",
-			destination: "",
-			sourceLocation: null,
-			destinationLocation: null,
-			path: [],
-			mapCenter: { lat: 18.5167, lng: 73.8561 },
-			currentPosition: null,
-			watchId: null,
-		};
+const LiveTraking = (props) => {
+	const [data, setData] = useState({
+		source: "",
+		destination: "",
+		sourceLocation: null,
+		destinationLocation: null,
+		path: [],
+		mapCenter: { lat: 18.5167, lng: 73.8561 },
+		currentPosition: null,
+		watchId: null,
+	});
+
+	const handleSetData = (name, value) => {
+		setData((prev) => {
+			return {
+				...prev,
+				[name]: value,
+			}
+		})
 	}
 
-	componentDidMount() {
+	useEffect(() => {
 		if (navigator.geolocation) {
 			const watchId = navigator.geolocation.watchPosition(
 				(position) => {
@@ -29,31 +36,33 @@ class LiveTraking extends Component {
 						lat: position.coords.latitude,
 						lng: position.coords.longitude,
 					};
-					this.setState({ currentPosition: currentLocation });
+					handleSetData("currentPosition", currentLocation);
 
-					if (this.state.sourceLocation) {
-						this.updatePath(this.state.sourceLocation, currentLocation);
+					if (data.sourceLocation) {
+						updatePath(data.sourceLocation, currentLocation);
 					}
 				}, (error) => {
 					console.error("Error getting current location:", error);
 				}
 			);
-			this.setState({ watchId });
+			handleSetData("watchId", watchId);
 		}
-	}
+	},[]);
 
-	componentWillUnmount() {
-		if (this.state.watchId) {
-			navigator.geolocation.clearWatch(this.state.watchId);
-		}
-	}
+	useEffect(() => {
+		return (() => {
+			if (data.watchId) {
+				navigator.geolocation.clearWatch(data.watchId);
+			}
+		});
+	}, []);
 
-	handleAutoComplete = (field) => (e) => {
+	const handleAutoComplete = (field) => (e) => {
 		const value = e.target.value;
-		this.setState({ [field]: value });
+		handleSetData(field, value);
 
 		if (value.trim() === "") {
-			this.setState({ [`${field}Location`]: null });
+			handleSetData(`${field}Location`, null);
 			return;
 		}
 
@@ -62,36 +71,34 @@ class LiveTraking extends Component {
 			{ input: value },
 			(predictions, status) => {
 				if (status === "OK" && predictions.length > 0) {
-				this.setState({
-					[`${field}Predictions`]: predictions,
-				});
+					handleSetData(`${field}Predictions`, predictions);
 				} else {
-				this.setState({ [`${field}Predictions`]: [] });
+					handleSetData(`${field}Predictions`, []);
 				}
 			}
 		);
 
 		if (field === "source" || field === "destination") {
-			if (this.state.watchId) {
-				navigator.geolocation.clearWatch(this.state.watchId);
-				this.setState({ watchId: null });
+			if (data.watchId) {
+				navigator.geolocation.clearWatch(data.watchId);
+				handleSetData("watchId", null);
 			}
 		}
 	};
 
-	selectPrediction = (field, prediction) => {
-		this.setState({ [field]: prediction.description });
-		this.setState({ [`${field}Predictions`]: [] });
+	const selectPrediction = (field, prediction) => {
+		handleSetData(field, prediction.description);
+		handleSetData(`${field}Predictions`, []);
 
 		const placesService = new window.google.maps.places.PlacesService(
 			document.createElement("div")
 		);
 		placesService.getDetails({ placeId: prediction.place_id }, (place) => {
-			this.setState({ [`${field}Location`]: place.geometry.location });
+			handleSetData(`${field}Location`, place.geometry.location);
 		});
 	};
 
-	useCurrentLocation = () => {
+	const useCurrentLocation = () => {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition((position) => {
 				const currentLocation = {
@@ -101,10 +108,8 @@ class LiveTraking extends Component {
 				const geocoder = new window.google.maps.Geocoder();
 				geocoder.geocode({ location: currentLocation }, (results, status) => {
 					if (status === "OK" && results[0]) {
-						this.setState({
-						source: results[0].formatted_address,
-						sourceLocation: currentLocation,
-						});
+						handleSetData("source", results[0].formatted_address);
+						handleSetData("sourceLocation", currentLocation);
 					} else {
 						console.error("Error fetching current location:", status);
 					}
@@ -115,31 +120,31 @@ class LiveTraking extends Component {
 		}
   	};
 
-	handleGetDirections = () => {
-		const { source, destination } = this.state;
+	const handleGetDirections = () => {
+		const { source, destination } = data;
 
 		if (source && destination) {
-			this.getDirections(source, destination);
-			if (this.state.sourceLocation) {
-				this.setState({ mapCenter: this.state.sourceLocation });
+			getDirections(source, destination);
+			if (data.sourceLocation) {
+				handleSetData("mapCenter", data.sourceLocation);
 			}
 		} else {
 			console.error("Source and destination addresses are required.");
 		}
 	};
 
-	getDirections = (source, destination) => {
+	const getDirections = (source, destination) => {
 		const DirectionsService = new window.google.maps.DirectionsService();
 
 		DirectionsService.route(
-		{
-			origin: source,
-			destination: destination,
-			travelMode: window.google.maps.TravelMode.DRIVING,
-		}, (result, status) => {
-			if (status === window.google.maps.DirectionsStatus.OK) {
-				const path = result.routes[0].overview_path;
-				this.setState({ path });
+			{
+				origin: source,
+				destination: destination,
+				travelMode: window.google.maps.TravelMode.DRIVING,
+			}, (result, status) => {
+				if (status === window.google.maps.DirectionsStatus.OK) {
+					const path = result.routes[0].overview_path;
+					handleSetData("path", path);
 				} else {
 					console.error("Error fetching directions:", status);
 				}
@@ -147,84 +152,85 @@ class LiveTraking extends Component {
 		);
 	};
 
-	updatePath = (source, currentLocation) => {
+	const updatePath = (source, currentLocation) => {
 		const DirectionsService = new window.google.maps.DirectionsService();
 
 		DirectionsService.route(
-		{
-			origin: source,
-			destination: currentLocation,
-			travelMode: window.google.maps.TravelMode.DRIVING,
-		}, (result, status) => {
-			if (status === window.google.maps.DirectionsStatus.OK) {
-				const path = result.routes[0].overview_path;
-				this.setState({ path });
+			{
+				origin: source,
+				destination: currentLocation,
+				travelMode: window.google.maps.TravelMode.DRIVING,
+			}, (result, status) => {
+				if (status === window.google.maps.DirectionsStatus.OK) {
+					const path = result.routes[0].overview_path;
+					handleSetData("path", path);
 				} else {
-				console.error("Error fetching directions:", status);
+					console.error("Error fetching directions:", status);
 				}
 			}
 		);
 	};
 
-	render() {
-		const {
-			source,
-			destination,
-			sourcePredictions,
-			destinationPredictions,
-			currentPosition,
-		} = this.state;
 
-		return (
-			<div style={{ width: "100%", height: "100%" }}>
-				<div className="flex flex-col p-5 items-center">
-					<div>
-						<div className="flex flex-row">
+	// render() {
+	// 	const {
+	// 		source,
+	// 		destination,
+	// 		sourcePredictions,
+	// 		destinationPredictions,
+	// 		currentPosition,
+	// 	} = this.state;
+
+	return (
+		<div style={{ width: "100%", height: "100%" }}>
+			<div className="flex flex-col p-5 items-center">
+				<div>
+					<div className="flex flex-row">
 						<input
 							type="text"
-							value={source}
-							onChange={this.handleAutoComplete("source")}
+							value={data.source}
+							onChange={handleAutoComplete("source")}
 							style={{
-							borderRadius: "15px",
-							border: "1px solid black",
-							padding: "5px",
-							width: "70vw",
+								borderRadius: "15px",
+								border: "1px solid black",
+								padding: "5px",
+								width: "70vw",
 							}}
 							placeholder="Source Address"
 						/>
 						<button
-							onClick={this.useCurrentLocation}
+							onClick={useCurrentLocation}
 							style={{
-							borderRadius: "15px",
-							border: "1px solid black",
-							padding: "5px",
-							width: "20vw",
+								borderRadius: "15px",
+								border: "1px solid black",
+								padding: "5px",
+								width: "20vw",
 							}}
 						>
 							Current Location
 						</button>
-						</div>
-						<ul
-						style={{
-							width: "100%",
-						}}
-						>
-						{sourcePredictions &&
-							sourcePredictions.map((prediction) => (
+					</div>
+					<ul
+					style={{
+						width: "100%",
+					}}
+					>
+						{data.sourcePredictions &&
+							data.sourcePredictions.map((prediction) => (
 							<li
 								key={prediction.place_id}
-								onClick={() => this.selectPrediction("source", prediction)}
+								onClick={() => selectPrediction("source", prediction)}
 							>
 								{prediction.description}
 							</li>
-							))}
-						</ul>
-					</div>
-					<div className="flex flex-col">
-						<input
+						))}
+					</ul>
+				</div>
+				<div className="flex flex-col">
+					<input
 						type="text"
-						value={destination}
-						onChange={this.handleAutoComplete("destination")}
+						value={data.destination}
+						onChange={handleAutoComplete("destination")}
 						style={{
 							borderRadius: "15px",
 							border: "1px solid black",
@@ -233,70 +239,69 @@ class LiveTraking extends Component {
 							height: "50px",
 						}}
 						placeholder="Destination Address"
-						/>
-						<ul>
-						{destinationPredictions &&
-							destinationPredictions.map((prediction) => (
-							<li
-								key={prediction.place_id}
-								onClick={() =>
-								this.selectPrediction("destination", prediction)
-								}
-							>
-								{prediction.description}
-							</li>
-							))}
-						</ul>
-					</div>
-					<button
-						onClick={this.handleGetDirections}
-						style={{
+					/>
+					<ul>
+					{data.destinationPredictions &&
+						data.destinationPredictions.map((prediction) => (
+						<li
+							key={prediction.place_id}
+							onClick={() =>
+								selectPrediction("destination", prediction)
+							}
+						>
+							{prediction.description}
+						</li>
+						))}
+					</ul>
+				</div>
+				<button
+					onClick={handleGetDirections}
+					style={{
 						borderRadius: "15px",
 						border: "1px solid black",
 						padding: "5px",
 						width: "30vw",
 						height: "50px",
-						}}
-					>
-						Get Directions
-					</button>
-				</div>
-				<Map
-					google={this.props.google}
-					zoom={10}
-					initialCenter={this.state.mapCenter}
+					}}
 				>
-					{this.state.sourceLocation && (
-						<Marker
-						position={this.state.sourceLocation}
+					Get Directions
+				</button>
+			</div>
+			<Map
+				google={props.google}
+				zoom={10}
+				initialCenter={data.mapCenter}
+			>
+				{data.sourceLocation && (
+					<Marker
+						position={data.sourceLocation}
 						name="Source"
 						title="Source"
-						/>
-					)}
-					{this.state.destinationLocation && (
-						<Marker
-						position={this.state.destinationLocation}
+					/>
+				)}
+				{data.destinationLocation && (
+					<Marker
+						position={data.destinationLocation}
 						name="Destination"
 						title="Destination"
-						/>
-					)}
-					{currentPosition && (
-						<Marker
-						position={currentPosition}
+					/>
+				)}
+				{data.currentPosition && (
+					<Marker
+						position={data.currentPosition}
 						name="Current Location"
 						title="Current Location"
-						/>
-					)}
-					<Polyline
-						path={this.state.path}
-						strokeColor="#0000FF"
-						strokeOpacity={0.8}
-						strokeWeight={2}
 					/>
-				</Map>
-			</div>
-		);
-	}
+				)}
+				<Polyline
+					path={data.path}
+					strokeColor="#0000FF"
+					strokeOpacity={0.8}
+					strokeWeight={2}
+				/>
+			</Map>
+		</div>
+	);
 }
 
 export default GoogleApiWrapper({
